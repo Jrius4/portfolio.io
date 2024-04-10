211c8c705afa1beb8c5e0f20b66b4c0566e324b3
Jrius Kaxibwe <kazibwejuliusjunior@gmail.com>
2024-04-10 04:25:30 +0300

    portfolio

---
+++ utils/data/contactsData.js
@@ -0,0 +1,12 @@
+export const contactsData = {
+  email: "juliusjuniorkazibwe@gmail.com",
+  phone: "+256779571619",
+  address: "Muyenga Kampala, Uganda",
+  github: "https://github.com/Jrius4",
+  facebook:
+    "https://www.facebook.com/profile.php?id=100006435786827&mibextid=ZbWKwL",
+  linkedIn: "www.linkedin.com/in/julius-junior-kazibwe-35920914a",
+  twitter: "https://twitter.com/JriusKazibwe",
+  stackOverflow: "https://stackoverflow.com/users/16840768/abu-said",
+  devUsername: "Jrius",
+};
